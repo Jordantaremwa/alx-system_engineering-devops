f9e280d608185ee86bab94843f4d057431c158e5
Taremwa Jordan <jordantalemwa3@gmail.com>
2024-03-16 08:16:50 -0700

    2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,16 +1,3 @@
 #!/usr/bin/env ruby
-unless ARGV.length == 1
-  puts "Usage: ruby 2-repetition_token_1.rb <string>"
-  exit 1
-end
-
-pattern = /hbt{2,5}n/
-
-input_string = ARGV[0]
-
-if input_string.match?(pattern)
-  puts "Match found: #{input_string}"
-else
-  puts "No match found."
-end
+puts ARGV[0].scan(/htn|hbtn/).join
 
